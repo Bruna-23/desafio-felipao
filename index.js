90b7e0487faf4bd65e3de85b86126c0dc0759d61
Bruna Scremim <brunasm.qa@gmail.com>
2024-02-17 17:50:20 -0300

    Escrevi todo o código proposto no desafio

---
+++ index.js
@@ -0,0 +1,22 @@
+let nomeHeroi = Spider-Man
+let xpHeroi = "7259"
+
+if (xpHeroi <1000) {
+    console.log ("Ferro")
+} else if (xpHeroi >1001 && <2000) {
+    console.log ("Bronze")
+} else if (xpHeroi >2001 && <5000) {
+    console.log ("Prata")
+}else if (xpHeroi >5001 && <7000) {
+    console.log ("Ouro")
+}else if (xpHeroi >7001 && <8000) {
+    console.log ("Platina")
+}else if (xpHeroi >8001 && <9000) {
+    console.log ("Ascendente")
+}else if (xpHeroi >9001 && <10000) {
+    console.log ("Imortal")
+    else {
+        console.log ("Radiante")
+    }
+
+    console.log ("O Herói de nome " + nomeHeroi + "está no nível de " xpHeroi)
